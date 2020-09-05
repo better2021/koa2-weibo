e270f3f59798e6d233445d48a92d8bfb5f2c97a3
coco520 <feiyu@tg10010.com>
2020-09-05 19:35:55 +0700

    'post'

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,38 @@
-const router = require('koa-router')()
+const router = require('koa-router')();
 
 router.get('/', async (ctx, next) => {
   await ctx.render('index', {
-    title: 'Hello Koa 2!'
-  })
-})
+    title: 'Hello Koa 2!',
+  });
+});
 
 router.get('/string', async (ctx, next) => {
-  ctx.body = 'koa2 string'
-})
+  ctx.body = 'koa2 string';
+});
 
 router.get('/json', async (ctx, next) => {
   ctx.body = {
-    title: 'koa2 json'
-  }
-})
+    title: 'koa2 json',
+  };
+});
 
-module.exports = router
+router.get('/profile/:userName/:pageIndex', async (ctx, next) => {
+  const { userName, pageIndex } = ctx.params;
+  ctx.body = {
+    tltle: 'this is profile page',
+    userName,
+    pageIndex,
+  };
+});
+
+router.post('/login', async (ctx, next) => {
+  const { userName, password } = ctx.request.body;
+  // 登录验证的逻辑
+  ctx.body = {
+    tag: 100,
+    userName,
+    password,
+  };
+});
+
+module.exports = router;
