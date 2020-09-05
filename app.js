b7f8574c8dab03bde4df24cfc642153485a24df2
coco520 <feiyu@tg10010.com>
2020-09-05 11:28:12 +0700

    'weibo'

---
+++ app.js
@@ -0,0 +1,50 @@
+const Koa = require('koa');
+const app = new Koa();
+const views = require('koa-views');
+const json = require('koa-json');
+const onerror = require('koa-onerror');
+const bodyparser = require('koa-bodyparser');
+const logger = require('koa-logger');
+
+const index = require('./routes/index');
+const users = require('./routes/users');
+
+// error handler
+onerror(app);
+
+// middlewares
+app.use(
+  bodyparser({
+    enableTypes: ['json', 'form', 'text'],
+  })
+);
+app.use(json());
+app.use(logger());
+
+// 静态化后的目录可以直接访问，例如：http://localhost:3000/stylesheets/style.css
+app.use(require('koa-static')(__dirname + '/public'));
+
+app.use(
+  views(__dirname + '/views', {
+    extension: 'ejs',
+  })
+);
+
+// logger
+app.use(async (ctx, next) => {
+  const start = new Date();
+  await next();
+  const ms = new Date() - start;
+  console.log(`${ctx.method} ${ctx.url} - ${ms}ms`);
+});
+
+// routes
+app.use(index.routes(), index.allowedMethods());
+app.use(users.routes(), users.allowedMethods());
+
+// error-handling
+app.on('error', (err, ctx) => {
+  console.error('server error', err, ctx);
+});
+
+module.exports = app;
