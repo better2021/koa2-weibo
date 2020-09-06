e14fbd00f3a929bf710044eb9d7e086cf3bf4896
coco520 <feiyu@tg10010.com>
2020-09-06 16:13:49 +0700

    'ejs'

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,27 @@ const router = require('koa-router')();
 router.get('/', async (ctx, next) => {
   await ctx.render('index', {
     title: 'Hello Koa 2!',
+    msg: '你好',
+    name: 'xiaoming',
+    isMe: true,
+    blogList: [
+      {
+        id: 1,
+        title: 'aaa',
+      },
+      {
+        id: 2,
+        title: 'bbb',
+      },
+      {
+        id: 3,
+        title: 'ccc',
+      },
+      {
+        id: 4,
+        title: 'ddd',
+      },
+    ],
   });
 });
 
