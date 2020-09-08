a307c7db655b909810875c4b1e8ba0cb84d06a34
coco520 <feiyu@tg10010.com>
2020-09-08 20:06:49 +0700

    'sync'

--- src/db/seq.js
+++ src/db/seq.js
@@ -5,13 +5,17 @@
 
 const Sequelize = require('sequelize');
 const { MYSQL_CONF } = require('../conf/db');
-const { isProd } = require('../utils/env');
+const { isProd, isTest } = require('../utils/env');
 
 const { host, user, password, database } = MYSQL_CONF;
 const conf = {
   host,
   dialect: 'mysql',
 };
+
+if (isTest) {
+  conf.logging = () => {};
+}
 
 //  线上环境使用连接池
 if (isProd) {
---
+++ src/db/sync.js
@@ -0,0 +1,23 @@
+/**
+ * @deprecated 同步数据库
+ */
+
+const seq = require('./seq');
+
+// require('./model');
+
+// 测试连接
+seq
+  .authenticate()
+  .then(() => {
+    console.log('auth ok');
+  })
+  .catch(() => {
+    console.log('auth err');
+  });
+
+// 执行同步
+seq.sync({ force: true }).then(() => {
+  console.log('sunc ok');
+  process.exit();
+});
